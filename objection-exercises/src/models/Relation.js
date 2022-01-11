b89666e4375d1ca0aa104a941d050b1d151ed1e3
Elizabeth Li <elizabethli@college.harvard.edu>
2022-01-11 11:34:20 -0500

    Trying commit

--- objection-exercises/src/models/Relation.js
+++ objection-exercises/src/models/Relation.js
@@ -1,1 +1,14 @@
 // Write your relation model here!
+
+const { HasManyRelations, ManytoManyRelation, OnetoManyRelation } = require('./BaseModel')
+const BaseModel = require('./BaseModel')
+
+class Relation extends BaseModel {
+  static get tableName() {
+    return 'relations'
+  }
+
+  static get relationMappings() {
+    return {}
+  }
+}
